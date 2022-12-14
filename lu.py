0e6554a1fc5046793c2b1952beadf1f51e73bfbd
kamsur <57803389+kamsur@users.noreply.github.com>
2022-12-14 16:57:06 +0100

    LU complete done

--- lu.py
+++ lu.py
@@ -1,40 +1,38 @@
 import numpy as np
 
-'''A = np.array([[2,7,1],
-             [3,-8,-1],
-             [1,5,3]])'''
-
 def lu(A):
     # TODO
-    U = None
-    L = None
     m = A.shape[0]
     U = A.astype(float)
-    L = np.identity(m)
+    L = np.identity(m).astype(float)
     for k in range (m-1):
-        for j in range (k+1,m):
-            L[j][k] = np.divide(U[j][k],U[k][k])
-            U[j][k:m] = U[j][k:m] - np.multiply(L[j][k],U[k][k:m])
+        L[k+1:,k]=np.divide(U[k+1:,k],U[k][k])
+        U[k+1:,k:]=U[k+1:,k:]-np.outer(L[k+1:,k],U[k,k:])
+
     return (L, U)
 
 def maxabs_idx(A):
     # TODO
-    i, j = (0, 0)
-    max=np.absolute(A[0][0])
-    (m,n) = A.shape
-    for k in range (m):
-        for l in range (n):
-             if np.absolute(A[k][l])>max:
-                (i,j)=(k,l)
-                max=np.absolute(A[k][l])
+    i,j=np.unravel_index(np.argmax(np.absolute(A)),A.shape)
     return (i, j)
 
 
 def lu_complete(A):
     # TODO
-    U = None
-    L = None
-    P = None
-    Q = None
+    A=A.astype(float)
+    U = A.copy()
+    m=A.shape[0]
+    L = np.eye(m).astype(float)
+    P = np.eye(m).astype(float)
+    Q = np.eye(m).astype(float)
+    for k in range(m-1):
+        max_i, max_j=np.array(maxabs_idx(U[k:,k:]))+k
+        U[[k,max_i],k:]=U[[max_i,k],k:]
+        U[:,[k,max_j]]=U[:,[max_j,k]]
+        L[[k,max_i],:k]=L[[max_i,k],:k]
+        P[[k,max_i],:]=P[[max_i,k],:]
+        Q[:,[k,max_j]]=Q[:,[max_j,k]]
+        L[k+1:,k]=np.divide(U[k+1:,k],U[k][k])
+        U[k+1:,k:]=U[k+1:,k:]-np.outer(L[k+1:,k],U[k,k:])
 
     return (P, Q, L, U)
